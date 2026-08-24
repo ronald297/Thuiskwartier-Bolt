5628eacbbe263312d3578961d01fa07537dcf67b
ronald297 <ronald@thuiskwartier.nl>
2026-08-24 09:31:00 +0200

    db in code

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vdnnpadkdgxzmwpiwill.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkbm5wYWRrZGd4em13cGl3aWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1NDI2NTgsImV4cCI6MjEwMzExODY1OH0.lYIEcF1DhLyKy3gLWPr7mlgw0LiWDpItD0bZlJt_zb8';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
